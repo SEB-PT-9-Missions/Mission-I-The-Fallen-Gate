82f15cddbff79c9dddc87cf3f03aabc1bd9ec182
Ali <ali.kamel5958@gmail.com>
2026-09-09 18:47:59 +0300

    added king route to welcome your majesty

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/gate', function (req, res) {
   res.send('🏰 The gates of Codoria are open!')
 })
 
+app.get('/king', function (req, res) {
+  res.send('👑 Welcome, Your Majesty!')
+})
+
 app.listen(3000, function () {
   console.log('⚔️ Royal server is running on port 3000')
 })
