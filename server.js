3ecfa334ad237509a2269abd885cb489a7130f8d
Ali <ali.kamel5958@gmail.com>
2026-09-09 18:45:05 +0300

    added prackets for calling express and set it to variable app, fixed the gate route, and fixed the .sent calling to .send

--- server.js
+++ server.js
@@ -10,10 +10,10 @@
 
 const express = require('express')
 
-const app = express
+const app = express()
 
-app.get('gate', function (req, res) {
-  res.sent('🏰 The gates of Codoria are open!')
+app.get('/gate', function (req, res) {
+  res.send('🏰 The gates of Codoria are open!')
 })
 
 app.listen(3000, function () {
